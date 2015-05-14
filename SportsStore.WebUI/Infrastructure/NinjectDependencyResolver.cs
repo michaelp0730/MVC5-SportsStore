de7d9d6f490aa34e8bb127a3cbf0e6a0ace065d9
Michael Pellegrini <mipelleg@microsoft.com>
2015-05-13 18:01:06 -0700

    Create mock repository

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -27,7 +31,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // TODO: put bindings here
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surfboard", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+            });
+            kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
         }
     }
 }
